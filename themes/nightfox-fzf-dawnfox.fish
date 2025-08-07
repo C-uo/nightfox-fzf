c26e9651a15bc05248f340853b76e19e1bf5ed25
C-uo <github@c-uo.dev>
2025-08-07 12:45:00 +0900

    feat: generate dawnfox themes for fish

---
+++ themes/nightfox-fzf-dawnfox.fish
@@ -0,0 +1,5 @@
+set -Ux FZF_DEFAULT_OPTS "\
+--color=fg:#575279,bg:#faf4ed,hl:#5ca7b4 \
+--color=fg+:#4c4769,bg+:#ebdfe4,hl+:#50848c \
+--color=info:#a8a3b3,prompt:#56949f,pointer:#56949f \
+--color=marker:#618774,spinner:#a8a3b3,header:#625c87"
